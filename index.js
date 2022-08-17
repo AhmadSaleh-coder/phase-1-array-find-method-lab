034f114c09c074f264bf37451f38ca3386dff072
Ahmad Saleh <ahmadrocks2011@hotmail.com>
2022-08-17 15:38:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+const record = [
+    { year: "2018", result: "L" },
+    { year: "2015", result: "W" },
+    { year: "2016", result: "N/A" }
+    //...
+]
+let year;
+
+
+const superbowlWin = record => {
+    
+    const result = record.find(record => record.result === "W");
+    if(result)
+        return result.year;
+
+    
+}
+
+console.log(superbowlWin(record));
